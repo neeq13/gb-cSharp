e66c3f972b4b91b108f8f2df7d14febc2441dd3f
Беллигор <neeq13@yandex.ru>
2023-02-23 18:57:57 +0300

    ДЗ№2

---
+++ les-2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Задача 1:");
+Console.Write("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вторая цифра: {a / 10 % 10}");
+
+Console.WriteLine("Задача 2:");
+Console.Write("Введите число: ");
+string b = Console.ReadLine();
+if (b.Length <= 2)
+    Console.WriteLine("Третьей цифры нет");
+else
+    Console.WriteLine($"Третья цифра: {b[2]}");
+
+Console.WriteLine("Задача 3:");
+Console.Write("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if ((day == 6) || (day == 7))
+    Console.Write("Выходной день");
+else
+    Console.Write("Рабочий день");
