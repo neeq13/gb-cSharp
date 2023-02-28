480b65d3ce8c607df381e917fcf0c627786f4d9c
Беллигор <neeq13@yandex.ru>
2023-02-28 18:31:51 +0300

    ДЗ№6

---
+++ les-6/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Задача 1:");
+Console.Write("Введите длинну массива: ");
+int arr = Convert.ToInt32(Console.ReadLine());
+
+void NegativeNumbers(int mass)
+{
+    int[] list = new int[mass];
+    int count = 0;
+    for (int i = 0; i < list.Length; i++)
+    {
+        list[i] = new Random().Next(-10, 10);
+        if (list[i] > 0) 
+        {
+            count++;
+        }
+    }
+    Console.WriteLine("[{0}]", string.Join(", ", list));
+    Console.WriteLine($"Количество чисел в массиве больше 'нуля' = {count}");
+}
+
+NegativeNumbers(arr);
