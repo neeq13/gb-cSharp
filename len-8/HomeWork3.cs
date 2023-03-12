ed4f63b3d7c06b4e92136b18188cb3a4d913792a
Беллигор <neeq13@yandex.ru>
2023-03-12 23:45:40 +0300

    правки

--- len-8/HomeWork3.cs
+++ len-8/HomeWork3.cs
@@ -1,8 +1,12 @@
 ﻿class HomeWork3
 {
 
-    private int[, ] doubleMass(int arrX, int arrY)
+    private int[, ] doubleMass()
     {
+        Console.Write("Размер матирцы по X: ");
+        int arrX = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Размер матирцы по Y: ");
+        int arrY = Convert.ToInt32(Console.ReadLine());
         int[,] list = new int[arrY, arrX];
         for (int i = 0; i < arrY; i++)
         {
@@ -21,7 +25,7 @@
             Console.Write("[");
             for(int j = 0; j < arr.GetLength(1); j++)
             {
-                Console.Write("{0, 3}", arr[i, j]);
+                Console.Write("{0, 4}", arr[i, j]);
             }
             Console.WriteLine("]");
         }
@@ -30,30 +34,47 @@
     private void matrixPrint(int[, ] list1, int[, ] list2)
     {
         Console.WriteLine("-- Заданные матрицы --");
-        for (int i = 0; i < list1.GetLength(0); i++)
+        int temp = list1.GetLength(0) > list2.GetLength(0) ? list1.GetLength(0) : list2.GetLength(0);
+        for (int i = 0; i < temp; i++)
         {
-            Console.Write("[");
-            for(int j = 0; j < list1.GetLength(1); j++)
+            if(i < list1.GetLength(0))
             {
-                Console.Write("{0, 3}", list1[i, j]);
+                Console.Write("[");
+                for(int j = 0; j < list1.GetLength(1); j++)
+                {
+                    Console.Write("{0, 3}", list1[i, j]);
+                }
+                Console.Write("]");
             }
-            Console.Write("]");
-            Console.Write(" [");
-            for(int j = 0; j < list1.GetLength(1); j++)
+            else
+                Console.Write("           ");
+            if(i > list2.GetLength(0))
+                continue;
+            else
             {
-                Console.Write("{0, 3}", list2[i, j]);
+                Console.Write(" [");
+                for(int j = 0; j < list2.GetLength(1); j++)
+                {
+                    
+                    Console.Write("{0, 3}", list2[i, j]);
+                }
+                Console.Write("]");
             }
-            Console.Write("]");
             Console.WriteLine();
         }
     }
 
     public void matrixMult()
     {
-        int[, ] A = doubleMass(2, 2);
-        int[, ] B = doubleMass(2, 2);
+        // int[, ] A = {{2, 4}, {3, 2}}; // Проверочный вариант
+        // int[, ] B = {{3, 4}, {3, 3}}; // Проверочный вариант
+        Console.WriteLine("Задайте параметры матриц, но учитывайти они должны быть ьлдбкл квадратными 3х3");
+        Console.WriteLine("Первая матирца: ");
+        int[, ] A = doubleMass();
+        Console.WriteLine("Вторая матирца: ");
+        int[, ] B = doubleMass();
         matrixPrint(A, B);
-        int[, ] C = new int[A.GetLength(1), A.GetLength(0)];
+        int[, ] C = new int[A.GetLength(0), A.GetLength(1)];
 
         Console.WriteLine();
         Console.WriteLine("-- Произведение матриц --");
@@ -61,8 +82,17 @@
         {
             for(int j = 0; j < C.GetLength(1); j++)
             {
-                C[i, j] = A[i, 0] * B[0, j] + A[i, 1] * B[1, j];
-                Console.WriteLine($"C[{i}, {j}] = {A[i, 0]} * {B[0, j]} + {A[i, 1]} * {B[1, j]} = {C[i, j]}");
+                Console.Write($"C[{i}, {j}] = ");
+                for(int k = 0; k < C.GetLength(0); k++)
+                {
+                    C[i, j] += A[i, k] * B[k, j];
+                    Console.Write($"{A[i, k]} * {B[k, j]}");
+                    if(k + 1 < C.GetLength(0))
+                    {
+                        Console.Write(" + ");
+                    }
+                }
+                Console.WriteLine($" = {C[i, j]}");
             }
         }
         print(C);
